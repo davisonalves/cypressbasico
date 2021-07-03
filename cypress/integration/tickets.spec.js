a96ab51f1d87bcdc1ffde61dbaba9bf6549686de
Davison Alves <53572219+davisonalves@users.noreply.github.com>
2021-07-03 17:35:22 -0400

    aula3

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -2,6 +2,15 @@ describe('Tickets', () => {
     beforeEach(() => {
         cy.visit("https://ticket-box.s3.eu-central-1.amazonaws.com/index.html");
     });
+    it("fills all the text input fields", () => {
+        const firstName = "Davison";
+        const lastName = "Silva";
+        cy.get("#first-name").type(firstName);
+        cy.get("#last-name").type(lastName);
+        cy.get("#email").type("davison@email.com");
+        cy.get("#requests").type("Vegetariano");
+        cy.get("#signature").type(`${firstName} ${lastName}`);
+    });
     it("has 'TICKETBOX' header's heading", () => {
         
     });
